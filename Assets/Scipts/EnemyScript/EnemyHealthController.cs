9af25c7573be4cca1c6a7eff1e24e4338dd2412f
rgglewerenz <43972874+rgglewerenz@users.noreply.github.com>
2025-12-08 18:44:30 -0500

    added in level 2 and 3 boilerplate, and created the bfg

--- Assets/Scipts/EnemyScript/EnemyHealthController.cs
+++ Assets/Scipts/EnemyScript/EnemyHealthController.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class EnemyHealthController : GenericHealthScript
 {
-    
-    
+    public GameObject deathEffectPrefab;
+
 
     protected override void Die()
     {
+        if (deathEffectPrefab != null)
+        {
+            var gameobject = Instantiate(deathEffectPrefab, transform.position, Quaternion.identity);
+            Destroy(gameobject, 3f);
+        }
         Destroy(this.gameObject);
     }
 }
